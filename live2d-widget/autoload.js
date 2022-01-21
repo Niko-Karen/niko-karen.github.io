d7272852ca0219ca01f49fd1ef3b55c88c6d654b
Niko-Karen <liuzx0636@outlook.com>
2022-01-21 19:01:38 +0800

    Site updated: 2022-01-21 19:01:28

---
+++ live2d-widget/autoload.js
@@ -0,0 +1,19 @@
+const live2d_path="https://cdn.jsdelivr.net/gh/Niko-Karen/live2d-widget/";function loadExternalResource(l,s){return new Promise((e,i)=>{let t;"css"===s?(t=document.createElement("link"),t.rel="stylesheet",t.href=l):"js"===s&&(t=document.createElement("script"),t.src=l),t&&(t.onload=()=>e(l),t.onerror=()=>i(l),document.head.appendChild(t))})}768<=screen.width&&Promise.all([loadExternalResource(live2d_path+"waifu.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",cdnPath:"https://unpkg.zhimg.com/akilar-live2dapi@latest/"})}),console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
